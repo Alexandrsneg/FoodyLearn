0e7791f9ecb4125959a9cca86b788ffc6d98e59e
Alexandr Ivanov <ai.kps42@gmail.com>
2021-12-01 18:18:51 +0700

    add rest models and layout

---
+++ app/src/main/java/com/example/foodylearn/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foodylearn
+
+object Constants {
+
+    const val API_KEY = "b0414841b9bd4c19ac60ce69cd737f97"
+    const val BASE_URL = "https://api.spoonacular.com"
+
+}
---
+++ app/src/main/java/com/example/foodylearn/models/Result.kt
@@ -0,0 +1,39 @@
+package com.example.foodylearn.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Result(
+    @SerializedName("aggregateLikes")
+    val aggregateLikes: Int,
+    @SerializedName("cheap")
+    val cheap: Boolean,
+    @SerializedName("dairyFree")
+    val dairyFree: Boolean,
+    @SerializedName("glutenFree")
+    val glutenFree: Boolean,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("readyInMinutes")
+    val readyInMinutes: Int,
+    @SerializedName("servings")
+    val servings: Int,
+    @SerializedName("sourceName")
+    val sourceName: String,
+    @SerializedName("sourceUrl")
+    val spoonacularSourceUrl: String,
+    @SerializedName("summary")
+    val summary: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("vegan")
+    val vegan: Boolean,
+    @SerializedName("vegetarian")
+    val vegetarian: Boolean,
+    @SerializedName("veryHealthy")
+    val veryHealthy: Boolean,
+    @SerializedName("weightWatcherSmartPoints")
+    val weightWatcherSmartPoints: Int
+)
