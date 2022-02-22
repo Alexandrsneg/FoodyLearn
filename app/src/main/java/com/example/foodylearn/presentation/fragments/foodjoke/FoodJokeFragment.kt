31ba7fc59c0b088fcb9a976052b8e29a84cb67b8
Alexandr <ai.kps42@gmail.com>
2022-02-22 18:02:39 +0700

    add button for ceck and enable notifications

--- app/src/main/java/com/example/foodylearn/presentation/fragments/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/example/foodylearn/presentation/fragments/foodjoke/FoodJokeFragment.kt
@@ -1,17 +1,11 @@
 package com.example.foodylearn.presentation.fragments.foodjoke
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.*
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.foodylearn.R
@@ -21,6 +15,7 @@ import com.example.foodylearn.models.FoodJoke
 import com.example.foodylearn.util.NetworkResult
 import com.example.foodylearn.viewmodels.MainViewModel
 import java.util.*
+
 
 class FoodJokeFragment : Fragment() {
 
@@ -62,6 +57,24 @@ class FoodJokeFragment : Fragment() {
             }
         }
 
+        if (!NotificationManagerCompat.from(requireContext()).areNotificationsEnabled())
+            binding.btnNotifications.visibility = View.VISIBLE
+
+        binding.btnNotifications.setOnClickListener {
+            val intent = Intent()
+            context?.let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                    intent.putExtra(Settings.EXTRA_APP_PACKAGE, it.packageName)
+                } else {
+                    intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+                    intent.putExtra("app_package", it.packageName)
+                    intent.putExtra("app_uid", it.applicationInfo.uid)
+                }
+                requireContext().startActivity(intent)
+            }
+        }
+
         return binding.root
     }
 
@@ -97,6 +110,14 @@ class FoodJokeFragment : Fragment() {
         } else {
             return super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!NotificationManagerCompat.from(requireContext()).areNotificationsEnabled())
+            binding.btnNotifications.visibility = View.VISIBLE
+        else
+            binding.btnNotifications.visibility = View.GONE
     }
 //
 //    @Composable
