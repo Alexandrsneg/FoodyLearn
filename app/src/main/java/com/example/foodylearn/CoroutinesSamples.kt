a5b84044fedd7f042fa50a0f68067d6c14e6f0ab
Alexandr <ai.kps42@gmail.com>
2022-11-28 12:03:47 +0300

    commit before module separating

--- app/src/main/java/com/example/foodylearn/CoroutinesSamples.kt
+++ app/src/main/java/com/example/foodylearn/CoroutinesSamples.kt
@@ -1,16 +1,26 @@
 package com.example.foodylearn
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.*
 
 fun main(): Unit = runBlocking {
-    var res1 = async {
-        longOperationA()
-    }
-    var res2 = async {
-        longOperationB()
-    }
-
-    println(res1.await() + res2.await())
+//    var res1 = async {
+//        longOperationA()
+//    }
+//    var res2 = async {
+//        longOperationB()
+//    }
+//
+//    println(res1.await() + res2.await())
+    var test = Test()
+    test.printPublic()
+    test.invokePrivatePrintData()
 }
 
 suspend fun longOperationA(): Int {
@@ -25,4 +35,47 @@ suspend fun longOperationB(): Int {
     delay(1000)
     println("longOperation B finish")
     return 1
+}
+
+inline fun <reified T> Activity.startActivity(block: (Intent)-> Unit) {
+    startActivity(
+        Intent(this, T::class.java).apply {
+            block(this)
+        }
+    )
+}
+
+ val ld = MutableLiveData<String>()
+ val _ld: LiveData<String> = ld
+
+inline fun <reified T> MutableLiveData<T>.changeState(block:(T)->T) {
+    value?.let {
+        val newState = block.invoke(it)
+        value = newState
+    }
+}
+
+fun doSmt(name: String) {
+    ld.changeState<String> {
+        val sm = 10 + 10
+        val newName = name + "ssss" + it
+        newName
+    }
+}
+
+inline fun <reified T>T.invokePrivatePrintData() {
+    T::class.java.declaredMethods.firstOrNull { it.name == "printPrivate" }?.let {
+        it.isAccessible = true
+        it.invoke(this)
+    }
+}
+
+class Test() {
+    private fun printPrivate() {
+        print("Print private")
+    }
+
+    fun printPublic() {
+        print("Print public")
+    }
 }
--- app/src/main/java/com/example/foodylearn/util/MyExtensionFunctions.kt
+++ app/src/main/java/com/example/foodylearn/util/MyExtensionFunctions.kt
@@ -1,5 +1,7 @@
 package com.example.foodylearn.util
 
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -11,4 +13,13 @@ fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observ
             observer.onChanged(t)
         }
     })
+}
+
+
+inline fun <reified T> Activity.startActivity(block: (Intent)-> Unit) {
+    startActivity(
+        Intent(this, T::class.java).apply {
+            block(this)
+        }
+    )
 }
