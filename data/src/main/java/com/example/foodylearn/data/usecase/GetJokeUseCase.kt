b5114a4d350348b7dc1f72ab8972fa9f3a123f31
alexandrSneg <ai.kpa42@gmail.com>
2023-11-22 15:49:39 +0300

    add Dispatcher injection

--- data/src/main/java/com/example/foodylearn/data/usecase/GetJokeUseCase.kt
+++ data/src/main/java/com/example/foodylearn/data/usecase/GetJokeUseCase.kt
@@ -6,6 +6,7 @@ import com.example.domain.usecase.IGetJokeUseCase
 import com.example.foodylearn.data.Constants
 import com.example.foodylearn.data.Repository
 import com.example.foodylearn.data.toFoodRecipesRest
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.lang.Exception
@@ -13,24 +14,24 @@ import javax.inject.Inject
 
 
 class GetJokeUseCase @Inject constructor(
-    private val repository: Repository
+    private val repository: Repository,
+    private val dispatcher: CoroutineDispatcher
 ) : IGetJokeUseCase {
     override suspend fun execute(apiKey: String): NetworkResult<String> {
         return try  {
-            withContext(Dispatchers.IO) {
-                val cachedJoke = repository.local.readJoke()
+            withContext(dispatcher) {
                 val response = repository.remote.getJoke(apiKey)
                 when {
                     !response.body()?.text.isNullOrBlank() ->
-                         NetworkResult.Success(response.body()!!.text!!)
+                        return@withContext NetworkResult.Success(response.body()!!.text!!)
                     response.errorBody() != null -> {
-                        cachedJoke?.let {
+                        repository.local.readJoke()?.let {
                             return@withContext NetworkResult.Success(it.joke.text ?: "Joke is empty...")
                         } ?: run {
                             return@withContext NetworkResult.Error(response.message())
                         }
                     }
-                    else -> NetworkResult.Error("Oops, try later")
+                    else -> return@withContext NetworkResult.Error("Oops, try later")
                 }
             }
         } catch (exception: Exception) {
